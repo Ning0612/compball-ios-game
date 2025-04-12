f18046aa03ff3cdea371f0c6e48953f841f26a56
Ning <lance930612@gmail.com>
2025-04-13 03:15:55 +0800

    Initial Commit

---
+++ CompBallTests/CompBallTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompBallTests.swift
+//  CompBallTests
+//
+//  Created by 王政甯 on 2025/4/13.
+//
+
+import Testing
+@testable import CompBall
+
+struct CompBallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
